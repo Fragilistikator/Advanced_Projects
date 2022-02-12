f72eed6ff193aa8b9aab6845882a1a53f601ca33
Fragilistikator <52839280+Fragilistikator@users.noreply.github.com>
2022-02-12 10:29:34 -0700

    Add files via upload

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,9 +3,14 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({ //data for schema
     title: String,
     directions: String,
+    name: String,
     creator: String,
     ingredients: [String],
     selectedFile: String,
+    likes: {
+        type: [String],
+        default: [],
+    },
     createdAt: {
         type: Date,
         default: new Date()
