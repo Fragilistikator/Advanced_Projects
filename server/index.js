4821d86d4133364a8a88cce1f4ccecbaf6e36f90
Fragilistikator <52839280+Fragilistikator@users.noreply.github.com>
2022-05-30 10:28:04 -0600

    Add files via upload
    Functionality for comments

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -13,13 +14,15 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //Setting siz
 app.use(cors());
 
 app.use('/posts', postRoutes); 
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Recipes API'); 
 })
 
-const PORT = process.env.PORT || 3000; //Saying what port we are testing on
+const PORT = process.env.PORT || 5000; //Saying what port we are testing on
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) //Listening and returning what port we are running on
     .catch((error) => console.log(error.message)); //Return error if it doesn't work
+    
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,9 +3,18 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({ //data for schema
     title: String,
     directions: String,
+    name: String,
     creator: String,
     ingredients: [String],
     selectedFile: String,
+    likes: {
+        type: [String],
+        default: [],
+    },
+    comments: { 
+        type: [String], 
+        default: [] 
+    },
     createdAt: {
         type: Date,
         default: new Date()
